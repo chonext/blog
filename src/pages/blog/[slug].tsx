51b430e9f5a01f3eda86f573f46042787d3bc0b9
Chocolate1999 <2816778271@qq.com>
2022-07-31 21:52:22 +0800

    fix: resolve chinese toc

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -108,7 +108,7 @@ export default function SingleBlogPage({
                 aspect={{ height: 2, width: 5 }}
               />
 
-              <h1 className='mt-4'>{frontmatter.title}</h1>
+              <h1 className='mt-4 text-3xl'>{frontmatter.title}</h1>
 
               <p className='mt-2 text-sm text-gray-600 dark:text-gray-300'>
                 Written on{' '}
