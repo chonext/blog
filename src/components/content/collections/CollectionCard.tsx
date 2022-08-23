7bea6cb2681508fc47bbdf5368f9c58e2e3565e2
Chocolate1999 <2816778271@qq.com>
2022-08-23 09:58:35 +0800

    fix: remove ;

--- src/components/content/collections/CollectionCard.tsx
+++ src/components/content/collections/CollectionCard.tsx
@@ -49,7 +49,6 @@ export default function CollectionCard({
           </UnstyledLink>
         </li>
       ))}
-      ;
     </ul>
   );
 }
--- src/components/content/projects/ProjectCard.tsx
+++ src/components/content/projects/ProjectCard.tsx
@@ -46,7 +46,6 @@ export default function ProjectCard({ project, className }: ProjectCardProps) {
           </UnstyledLink>
         </li>
       ))}
-      ;
     </ul>
   );
 }
