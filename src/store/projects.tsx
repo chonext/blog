781f175b3187e517e1d1c7cf92fc7315910abc8e
Chocolate1999 <2816778271@qq.com>
2022-09-25 17:13:05 +0800

    feat: add all project

--- src/store/projects.tsx
+++ src/store/projects.tsx
@@ -1,5 +1,12 @@
 import { BiCarousel } from 'react-icons/bi';
-import { SiNextdotjs } from 'react-icons/si';
+import { CgNotes, CgWebsite } from 'react-icons/cg';
+import {
+  SiHexo,
+  SiLeetcode,
+  SiMarkdown,
+  SiNextdotjs,
+  SiShopify,
+} from 'react-icons/si';
 import { atom } from 'recoil';
 
 export const projectsAtom = atom({
@@ -25,6 +32,63 @@ export const projectsAtom = atom({
           description: 'a demo for customer carousel case use swiper and slick',
           link: 'https://github.com/Chocolate1999/customer-carousel-case',
           icon: <BiCarousel />,
+        },
+        {
+          title: 'docusaurus-docs',
+          description:
+            'Little lion front-end programming growth learning document with docusaurus',
+          link: 'https://github.com/LionCubFrontEnd/docs',
+          icon: <SiMarkdown />,
+        },
+      ],
+    },
+    {
+      category: 'Vue Ecosystem',
+      child: [
+        {
+          title: 'vue-shop',
+          description:
+            'Vue family bucket development e-commerce management system (Element-UI)',
+          link: 'https://github.com/Chocolate1999/vue-shop',
+          icon: <SiShopify />,
+        },
+        {
+          title: 'imitation meituan website',
+          description:
+            'The latest Vue family bucket + SSR + Koa2 full stack development in 2020',
+          link: 'https://github.com/Chocolate1999/Vue-family-bucket-SSR-Koa2-full-stack-development-from-Meituan',
+          icon: <CgWebsite />,
+        },
+      ],
+    },
+    {
+      category: 'Hexo Ecosystem',
+      child: [
+        {
+          title: 'hexo-blog-lionkk',
+          description:
+            'Magic modified from butterfly theme, providing complete and detailed documentation',
+          link: 'https://github.com/Chocolate1999/hexo-blog-lionkk',
+          icon: <SiHexo />,
+        },
+      ],
+    },
+    {
+      category: 'Notes',
+      child: [
+        {
+          title: 'front-end-learning-to-organize-notes',
+          description:
+            'front-end knowledge system, more efficient absorption of experience and results',
+          link: 'https://github.com/Chocolate1999/Front-end-learning-to-organize-notes',
+          icon: <CgNotes />,
+        },
+        {
+          title: 'leetcode-javascript',
+          description:
+            'The JavaScript problem solving warehouse of LeetCode, the front-end brushing route (mind map)',
+          link: 'https://github.com/Chocolate1999/leetcode-javascript',
+          icon: <SiLeetcode />,
         },
       ],
     },
