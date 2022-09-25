7c10d3e197806ec7a558af75cdb529a257418d45
Chocolate1999 <2816778271@qq.com>
2022-09-25 10:43:28 +0800

    feat: add carousel case demo

--- src/store/projects.tsx
+++ src/store/projects.tsx
@@ -1,5 +1,5 @@
-import { BsGithub } from 'react-icons/bs';
-import { SiBilibili, SiNextdotjs, SiVite } from 'react-icons/si';
+import { BiCarousel } from 'react-icons/bi';
+import { SiNextdotjs } from 'react-icons/si';
 import { atom } from 'recoil';
 
 export const projectsAtom = atom({
@@ -13,42 +13,18 @@ export const projectsAtom = atom({
           description:
             'The most beautiful blog in modern times, using Next.js, TypeScript, Tailwind CSS, Welcome to visit',
           link: 'https://github.com/Chocolate1999/nextjs-tailwind-blog',
-          icon: <BsGithub />,
-        },
-        {
-          title: 'nextjs-tailwind-blog2',
-          description:
-            'The most beautiful blog in modern times, using Next.js, TypeScript, Tailwind CSS, Welcome to visit',
-          link: 'https://github.com/Chocolate1999/nextjs-tailwind-blog',
-          icon: <SiBilibili />,
-        },
-        {
-          title: 'nextjs-tailwind-blog3',
-          description:
-            'The most beautiful blog in modern times, using Next.js, TypeScript, Tailwind CSS, Welcome to visit',
-          link: 'https://github.com/Chocolate1999/nextjs-tailwind-blog',
-          icon: <SiVite />,
-        },
-        {
-          title: 'nextjs-tailwind-blog4',
-          description:
-            'The most beautiful blog in modern times, using Next.js, TypeScript, Tailwind CSS, Welcome to visit',
-          link: 'https://github.com/Chocolate1999/nextjs-tailwind-blog',
           icon: <SiNextdotjs />,
         },
+      ],
+    },
+    {
+      category: 'React Ecosystem',
+      child: [
         {
-          title: 'nextjs-tailwind-blog5',
-          description:
-            'The most beautiful blog in modern times, using Next.js, TypeScript, Tailwind CSS, Welcome to visit',
-          link: 'https://github.com/Chocolate1999/nextjs-tailwind-blog',
-          icon: <SiNextdotjs />,
-        },
-        {
-          title: 'nextjs-tailwind-blog6',
-          description:
-            'The most beautiful blog in modern times, using Next.js, TypeScript, Tailwind CSS, Welcome to visit',
-          link: 'https://github.com/Chocolate1999/nextjs-tailwind-blog',
-          icon: <SiNextdotjs />,
+          title: 'customer-carousel-case',
+          description: 'a demo for customer carousel case use swiper and slick',
+          link: 'https://github.com/Chocolate1999/customer-carousel-case',
+          icon: <BiCarousel />,
         },
       ],
     },
